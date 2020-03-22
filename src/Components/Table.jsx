361baac2031066f5aff295dcf580d6ebe5aa04a2
kyleruschel <dkruschel1125@gmail.com>
2020-03-22 10:15:48 -0600

    moving files around

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,18 +1,61 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './Table.css';
 
-const Table = ({ data, setSearch, onClick, val }) => {
-    const [tableData, setTableData] = useState();
+const Table = () => {
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState('');
+    const [val, setVal] = useState('');
+    const filtered = data.map(e => e)
+        .filter(e => e.name.toLowerCase()
+            .replace(/[^\w]/g, '')
+            .includes(search.toLowerCase()
+                .replace(/[^\w]/g, '')));
 
     useEffect(() => {
-        setTableData(data);
-    }, [data]);
+        axios.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json').then(res => {
+            setData(res.data.pokemon);
+        })
+    }, []);
+
+    useEffect(() => {
+        searchedData();
+    }, [search])
+
+    const searchedData = () => {
+        if (filtered.length) {
+            setData(filtered);
+        }
+    }
+
+    const searched = e => {
+        setVal(e);
+        setSearch(e);
+    }
+
+    const onClick = () => {
+        axios.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json').then(res => {
+            setData(res.data.pokemon);
+        })
+        setVal('');
+    }
+
+    const loading = () => {
+        return (
+            <div className='spinnerContainer'>
+                <div className="spinner-border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>
+        );
+    };
 
     return (
         <div className='rootContainer'>
             <div className='headerContainer'>
                 <div>
-                    <h1>Pokédex Table</h1>
+                    <h1 className='pokedexHeader'>Pokédex Table</h1>
                     <div className='searchContainer'>
                         <input
                             className="form-control"
@@ -20,8 +63,10 @@ const Table = ({ data, setSearch, onClick, val }) => {
                             aria-describedby="inputGroup-sizing-sm"
                             placeholder='Search a name here'
                             type='text' value={val || ''}
-                            onChange={e => setSearch(e.target.value)} />
+                            onChange={e => searched(e.target.value)} />
                         <button type="button" className="btn btn-primary" onClick={onClick}>Clear Search</button>
+                    </div>
+                    <div>
                     </div>
                 </div>
             </div>
@@ -37,7 +82,7 @@ const Table = ({ data, setSearch, onClick, val }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {tableData && tableData.map((e, i) => (
+                        {data.map((e, i) => (
                             <tr key={i}>
                                 <td className='pokemonNum'>
                                     {e.num}
--- src/Pages/Pokedex/Pokedex.jsx
+++ src/Pages/Pokedex/Pokedex.jsx
@@ -4,47 +4,9 @@ import './Pokedex.css';
 import Table from '../../Components/Table';
 
 const Pokedex = () => {
-  const [data, setData] = useState([]);
-  const [search, setSearch] = useState('');
-  const [val, setVal] = useState('');
-  const filtered = data.map(e => e)
-    .filter(e => e.name.toLowerCase()
-      .replace(/[^\w]/g, '')
-      .includes(search.toLowerCase()
-        .replace(/[^\w]/g, '')));
-
-  useEffect(() => {
-    axios.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json').then(res => {
-      setData(res.data.pokemon);
-    })
-  }, []);
-    
-  useEffect(() => {
-    searchedData();
-  }, [search])
-  
-  const searchedData = () => {
-    if (filtered.length) {
-      setData(filtered);
-    }
-  }
-  
-  const searched = e => {
-    setVal(e);
-    setSearch(e);
-  }
-
-  const onClick = () => {
-    axios.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json').then(res => {
-      setData(res.data.pokemon);
-    })
-    setVal('');
-  }
-
-
   return (
     <div className='mainRoot'>
-      <Table data={data} val={val} setSearch={searched} onClick={onClick} />
+      <Table />
     </div>
   )
 }
